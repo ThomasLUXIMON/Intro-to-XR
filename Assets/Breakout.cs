2d83d20f4836739a7eb467c13a41232c6161e0df
Thomas LUXIMON <tluximon24@student.oulu.fi>
2024-01-30 14:05:06 +0200

    qwe

--- Assets/Breakout.cs
+++ Assets/Breakout.cs
@@ -1,18 +1,22 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Breakout : MonoBehaviour
 {
     Vector3 Initial_position;
     Vector3 Position_A;
 
+    public InputActionReference action;
     public void Start()
     {
+        action.action.Enable();
+        action.action.performed += (ctx) =>
         Initial_position = transform.position;
         Position_A = new Vector3(0f, 50, 0f);
         }
     public void Update()
     {
-        if (Input.GetKeyDown("Button.Two"))
+        //if (Input.GetKeyDown("Button.Two"))
         {
             if (transform.position == Position_A)
             {
--- Assets/lightSCRIPT.cs
+++ Assets/lightSCRIPT.cs
@@ -1,18 +1,22 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.UIElements;
 
 public class lightSCRIPT : MonoBehaviour
 {
+    public InputActionReference action;
     public Light light;
     //
     void Start()
     {
+        action.action.Enable();
+        action.action.performed += (ctx) =>
         light = GetComponent<Light>();
     }
     void Update()
     {
 
-        if (Input.GetKeyDown("Button.One"))
+        // if (Input.GetKeyDown("Button.One"))
         {
             {
                 
